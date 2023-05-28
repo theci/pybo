17c64774030da1b0cd54f670b895cf821ed1fd63
창훈 <p16331751@gmail.com>
2023-05-28 22:38:54 +0900

    settins change

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,7 +15,7 @@ from pathlib import Path
 AUTH_USER_MODEL = "users.User"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 TEMPLATES_DIR = BASE_DIR / "templates"
 
 # Quick-start development settings - unsuitable for production
